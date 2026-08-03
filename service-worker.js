b857a8197d1467643d713a74783fd52b2d25875b
ous <ousmvne78@gmail.com>
2026-08-03 02:21:23 +0200

    fix: keep adhkar reader controls fixed

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "murajaah-flash";
-const CACHE_VERSION = "v123-adhkar-gold-border";
+const CACHE_VERSION = "v124-adhkar-fixed-reader";
 const APP_CACHE = `${CACHE_PREFIX}-app-${CACHE_VERSION}`;
 const MUSHAF_CACHE = `${CACHE_PREFIX}-mushaf-${CACHE_VERSION}`;
 
@@ -16,7 +16,7 @@ const APP_SHELL = [
   "./css/review-intro.css?v=5",
   "./css/typography.css?v=5",
   "./css/visual-audit.css?v=6",
-  "./css/design-system.css?v=63",
+  "./css/design-system.css?v=64",
   "./js/pwa.js",
   "./js/icon-system.js?v=31",
   "./js/app.js?v=adhkar-shortcut2",
